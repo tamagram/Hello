f57eac47836893a4db0c39c77e3b1c48870e8b56
tama <asupaka11235@icloud.com>
2020-08-01 03:48:40 +0900

    7/31

---
+++ problems/Python3/#11/ITP1_11_A.py
@@ -0,0 +1,38 @@
+# classを使う
+class dice:
+    def __init__(self, face):
+        self.face = face
+
+    def __call__(self):
+        print(self.face[0])
+
+    def rotate(self, controll):
+        
+        #N = [1, 5, 2, 3, 0, 4]
+        #S = [4, 0, 2, 3, 5, 1]
+        #E = [3, 1, 0, 5, 4, 2]
+        #W = [2, 1, 5, 0, 4, 3]
+
+        for swap in controll:
+            if swap == "N":
+                l=self.face
+                self.face=[l[1],l[5],l[2],l[3],l[0],l[4]]
+            elif swap == "S":
+                l=self.face
+                self.face=[l[4],l[0],l[2],l[3],l[5],l[1]]
+            elif swap == "E":
+                l=self.face
+                self.face=[l[3],l[1],l[0],l[5],l[4],l[2]]
+            elif swap == "W":
+                l=self.face
+                self.face=[l[2],l[1],l[5],l[0],l[4],l[3]]
+
+
+# インスタンスaのfaceに出目のlistを割り当てる
+a = dice(list(map(int, input().split())))
+
+controll = input()
+
+a.rotate(controll)
+
+a()
